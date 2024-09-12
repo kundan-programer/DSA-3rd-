11d478624d9ae6aa4e0b55e2c01e478fddf10782
Kundan-programmer <129212364+kundan-programer@users.noreply.github.com>
2024-09-12 14:38:12 +0530

    04-Calculator

--- 02-first-java/two_numberSum_04.java
+++ 02-first-java/two_numberSum_04.java
@@ -1,5 +1,37 @@
-package 02-first-java;
+import java.util.Scanner;
 
 public class two_numberSum_04 {
-  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the 1st number: ");
+    int num1 = sc.nextInt();
+
+    System.out.print("Enter the 2nd number: ");
+    int num2 = sc.nextInt();
+
+    int output = 0;
+
+    System.out.print("Choose the operator ==> ['+' ,'-', 'x' , '/'] ");
+    char operator = sc.next().charAt(0);
+
+    switch (operator) {
+      case '+':
+        output = num1 + num2;
+        break;
+      case '-':
+        output = num1 - num2;
+        break;
+      case '*':
+        output = num1 * num2;
+        break;
+      case '/':
+        output = num1 / num2;
+        break;
+
+      default:
+        System.out.println("Error...");
+        break;
+    }
+    System.out.print("The output is: " + output);
+  }
 }
